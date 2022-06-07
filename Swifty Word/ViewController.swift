32cad11ef0874405b686b5f45422e71a5638e565
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-06-07 22:41:47 +0700

    Initial Commit

---
+++ Swifty Word/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty Word
+//
+//  Created by TheGIZzz on 7/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
